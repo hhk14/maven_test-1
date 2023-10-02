d0c80dab30b491638d3cd54a153bea53f36315a6
hhk14 <104987812+hhk14@users.noreply.github.com>
2023-10-02 18:59:14 +0530

    Update dependency-check.sh

--- dependency-check.sh
+++ dependency-check.sh
@@ -8,11 +8,11 @@ CACHE_DIRECTORY="$DC_DIRECTORY/data/cache"
 
 if [ ! -d "$DATA_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $DATA_DIRECTORY"
-    mkdir -p "$DATA_DIRECTORY"
+    sudo mkdir -p "$DATA_DIRECTORY"
 fi
 if [ ! -d "$CACHE_DIRECTORY" ]; then
     echo "Initially creating persistent directory: $CACHE_DIRECTORY"
-    mkdir -p "$CACHE_DIRECTORY"
+    sudo mkdir -p "$CACHE_DIRECTORY"
 fi
 
 # Make sure we are using the latest version
